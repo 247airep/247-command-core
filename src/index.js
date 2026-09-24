353de2fc23926a890ef1f04eece4be0d2d9f13ee
247airep <247airep@gmail.com>
2026-09-24 19:13:08 -0400

    Export XLSX ingestion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { ingestCsv } from './ingest.js';
+export { ingestXlsx } from './ingest-xlsx.js';
 export { analyzeAgreements } from './analyze.js';
 export { buildPriorityReview } from './review.js';
 export { createOutcomeRecord, closeOutcomeRecord } from './outcome.js';
